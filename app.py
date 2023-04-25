b85ca05240012beefbc408d90f4c6ecc8e51be92
Bartosz Rzycki <125658269+barto-official@users.noreply.github.com>
2023-04-25 20:03:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from IPython.display import display, Javascript
 from urllib.parse import quote
 
 
-client_id = 'fe29fbb9fa7f4cf4b88ebf90faaeb562'
-client_secret = '4a50ecae553e45d8b80f0cb1a3fe92d9'
+client_id = st.secrets["Client_id"]
+client_secret = st.secrets["Client_secret"]
 
 # Set page configuration
 st.set_page_config(
@@ -31,9 +31,20 @@ scope = 'user-read-playback-state,user-modify-playback-state,user-read-private'
 sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
 #token_info = []#sp_oauth.get_cached_token()
 
+st.title('Spotify Recommender System')
+st.subheader("How it works: \n")
+st.write(""" 
+        1. Click Login button below to Log into our Spotify.
+        2. You may skip the first step but you won't be able to play music.
+        3. The Login page will redirect you.
+        4. Copy the URL of the redirected page.
+        5. Type the name of your favorite artits.
+        6. The system will provide 4 Recommended Artist and top songs for each of them.
+        7. Play Music and Enjoy!\n\n
+        """)
 
 auth_url = sp_oauth.get_authorize_url()
-st.write("Please log in to Spotify using the following link:")
+#st.write("Please log in to Spotify using the following link:")
 st.markdown(f"<a href='{auth_url}' target='_blank'>Log in with Spotify</a>", unsafe_allow_html=True)
 response_url = st.text_input("Enter the URL you were redirected to:")
 code = sp_oauth.parse_response_code(response_url)
@@ -70,6 +81,19 @@ def play_track(access_token, track_uri):
         st.audio(preview_url)
     else:
         st.write(f"Failed to start playing the track. Status code: {response.status_code}")
+def find_similar_artists(artist_name, num_artists=5):
+    # Search for the artist
+    result = sp.search(q='artist:' + artist_name, type='artist')
+    items = result['artists']['items']
+
+    if len(items) == 0:
+        return []
+
+    # Get the similar artists
+    artist_id = items[0]['id']
+    related_artists = sp.artist_related_artists(artist_id)['artists']
+    return [related_artist['name'] for related_artist in related_artists[:num_artists]]
+
 
 # Streamlit app code
 st.markdown(
@@ -125,42 +149,49 @@ st.markdown('<div class="title">Spotify Artist Search</div>', unsafe_allow_html=
 # Get user input for artist name
 st.markdown('<div class="input-container">', unsafe_allow_html=True)
 artist_name = st.text_input("Enter artist name:")
+similar_artists = find_similar_artists(artist_name)
 st.markdown('</div>', unsafe_allow_html=True)
 
-if artist_name:
-    # Search for artist
-    results = sp.search(q='artist:' + artist_name, type='artist')
-    items = results['artists']['items']
+for artist_name in similar_artists[1:]:
+    if artist_name:
+        # Search for artist
+        results = sp.search(q='artist:' + artist_name, type='artist')
+        items = results['artists']['items']
 
-    if len(items) > 0:
-        # Get top tracks of artist
-        artist_id = items[0]['id']
-        top_tracks = sp.artist_top_tracks(artist_id)
+        if len(items) > 0:
+            # Get top tracks of artist
+            artist_id = items[0]['id']
+            top_tracks = sp.artist_top_tracks(artist_id)
 
-        # Output top tracks
-        with st.container():
-            st.markdown('<div class="track-container">', unsafe_allow_html=True)
-            for track in top_tracks['tracks']:
-                track_info = track
-                track_name = track_info['name']
-                track_uri = track_info['uri']
-                track_image_url = track_info['album']['images'][0]['url']
-                preview_url = track_info['preview_url']
+            # Output top tracks
+            with st.container():
+                st.markdown('<div class="track-container">', unsafe_allow_html=True)
+                st.write('Author: ', artist_name)
+                i = 0
+                for track in top_tracks['tracks']:
+                    if i <= 5:
+                        i += 1
+                        track_info = track
+                        track_name = track_info['name']
+                        track_uri = track_info['uri']
+                        track_image_url = track_info['album']['images'][0]['url']
+                        preview_url = track_info['preview_url']
 
-                with st.container():
-                    #st.image(track_info['album']['images'][0]['url'], width=300)
-                    #st.write(track_name)
-                    if preview_url is not None:
-                        # Get the Spotify player widget using the Spotify URI
-                        spotify_widget_uri = f"https://open.spotify.com/embed/track/{track_uri.split(':')[-1]}"
+                        with st.container():
+                            if preview_url is not None:
+                                # Get the Spotify player widget using the Spotify URI
+                                spotify_widget_uri = f"https://open.spotify.com/embed/track/{track_uri.split(':')[-1]}"
 
-                        # Create the iframe element with the Spotify player widget
-                        spotify_player_html = f"""
-                            <iframe src="{spotify_widget_uri}" width="100%" height="80%" frameborder="0" allowtransparency="true" allow="encrypted-media" style="min-width: 250px; max-width: 440px;"></iframe>
-                        """
+                                # Create the iframe element with the Spotify player widget
+                                spotify_player_html = f"""
+                                    <iframe src="{spotify_widget_uri}" width="100%" height="80%" frameborder="0" allowtransparency="true" allow="encrypted-media" style="min-width: 250px; max-width: 440px;"></iframe>
+                                """
 
-                        # Display the Spotify player widget in your Streamlit app
-                        html(spotify_player_html)
-                    else:
-                        st.write('No preview available')
-            st.markdown('</div>', unsafe_allow_html=True)
+                                # Display the Spotify player widget in your Streamlit app
+                                html(spotify_player_html)
+                            else:
+                                st.write('No preview available')
+                    st.markdown('</div>', unsafe_allow_html=True)
+
+    else:
+        st.write("No similar artists found.")
